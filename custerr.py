75c2ba2f90c624db738b7297ab43aef0feae9899
Alex Tavarez <alexjtavarez@tutanota.com>
2022-07-27 10:05:47 -0400

    Altered indentation

--- custerr.py
+++ custerr.py
@@ -1,29 +1,30 @@
 class TypeConstraintError(TypeError):
-    def __init__(self, var_entered, type_required):
-        if isinstance(type_required, tuple):
-            new_str = ''
-            last = type_required[-1]
-            type_required_mod = type_required[0:len(type_required)-1]
-            for t in tuple:
-                new_str += t.__name__ + ', '
+  def __init__(self, var_entered, type_required):
+    if isinstance(type_required, tuple):
+      new_str = ''
+      last = type_required[-1]
+      type_required_mod = type_required[0:len(type_required)-1]
+      
+      for t in tuple:
+        new_str += t.__name__ + ', '
 
-            new_str += 'or ' + last.__name__
-        else:
-            type_required = type_required.__name__
+      new_str += 'or ' + last.__name__
+    else:
+      type_required = type_required.__name__
 
-        self.message = str(var_entered) + ' is constrained to type ' + type_required
-        super().__init__(message)
+    self.message = str(var_entered) + ' is constrained to type ' + type_required
+    super().__init__(message)
 
 class UnallowableAttributeError(ValueError):
-    def __init__(self, val):
-        self.message = '"' + val + '" is not an allowed attribute'
-        super().__init__(message)
+  def __init__(self, val):
+    self.message = '"' + val + '" is not an allowed attribute'
+    super().__init__(message)
 
 class UnmutableAttributeError(AttributeError):
-    def __init__(self, msg = 'New attribute cannot be created'):
-        self.message = msg
-        super().__init__(message)
+  def __init__(self, msg = 'New attribute cannot be created'):
+    self.message = msg
+    super().__init__(message)
 
 class IllegalMethodError(NotImplementedError):
-    def __init__(self, msg = 'This method call is illegal'):
-        self.message = msg
+  def __init__(self, msg = 'This method call is illegal'):
+    self.message = msg
