4ec3a36dbd726459eec2c4f44c4fc4ee864227a0
Alex Tavarez <alexjtavarez@tutanota.com>
2022-07-27 10:05:38 -0400

    Altered indentation

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import json
 
 allowed_exports = {
-    '^[Jj][Aa]?[Ss][Oo][Nn]$': {
-        'dump': json.dumps,
-        'load': json.loads
-      }
+  '^[Jj][Aa]?[Ss][Oo][Nn]$': {
+    'dump': json.dumps,
+    'load': json.loads
   }
+}
