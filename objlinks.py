2507efdc948460e36518d7049bda94066385e66a
Alex Tavarez <alexjtavarez@tutanota.com>
2022-07-31 12:21:52 -0400

    Created more classes, improved cond. cases and except. handling

--- objlinks.py
+++ objlinks.py
@@ -1,9 +1,14 @@
 # external modules
 from validators import url as validate
 from re import search as match
+from varname import nameof
+import datetime as dt
+import langcodes as lang
 
 # internal modules
 from generic import APObject, Link
+from actors import Group, Application, Organization, Person, Service
+from custerr import *
 
 class Note(APObject):
   def __init__(self, cntxt, identi, content):
@@ -11,6 +16,8 @@ class Note(APObject):
 
     if isinstance(content, str):
       self.content = content
+    else:
+      raise TypeConstraintError(nameof(content), str)
 
 class Article(APObject):
   def __init__(self, cntxt, identi, by):
@@ -19,20 +26,45 @@ class Article(APObject):
     if isinstance(content, str):
       self.content = content
 
-    if isinstance(by, str):
+    oftype = (str, Link, Group, Application, Organization, Person, Service)
+    if isinstance(by, oftype):
       self.attributedTo = by
+    elif isinstance(by, list):
+      for auth in by:
+        if isinstance(auth, oftype):
+          self.attributedTo = by
+          break
+        else:
+          raise TypeConstraintError(nameof(auth), oftype)
+        
 
 class Event(APObject):
   def __init__(self, cntxt, identi, start, end):
     super().__init__(cntxt, identi)
 
     if isinstance(start, str):
-      # TODO: add regex pattern check for xsd:dateTime format
+      xsddtstr = '^(-)?[0-9]+\-[0-9]{1,2}\-[0-9]{1,2}T[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}Z?$'
+      
+      if match(start, xsddtstr) is not None:
+        self.startTime = start
+      else:
+        raise InvalidXSDError(nameof(start))
+    elif isinstance(start, dt.datetime):
       self.startTime = start
+    else:
+      raise TypeConstraintError(nameof(start), (str, dt.datetime))
 
     if isinstance(end, str):
-      # TODO: add regex pattern check for xsd:dateTime format
+      xsddtstr = '^(-)?[0-9]+\-[0-9]{1,2}\-[0-9]{1,2}T[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}Z?$'
+      
+      if match(end, xsddtstr) is not None:
+        self.endTime = end
+      else:
+        raise InvalidXSDError(nameof(end))
+    elif isinstance(end, dt.datetime):
       self.endTime = end
+    else:
+      raise TypeConstraintError(nameof(end), (str, dt.datetime))
 
 class Document(APObject):
   def __init__(self, cntxt, identi, name, url):
@@ -40,6 +72,15 @@ class Document(APObject):
 
     if isinstance(name, str):
       self.name = name
+    elif isinstance(name, dict):
+      self.nameMap = {}
+      for k in name:
+        if lang.tag_is_valid(k):
+          self.nameMap[k] = name[k]
+        else:
+          raise ValueError('Invalid language code')
+    else:
+      raise TypeConstraintError(nameof(name), (str, dict))
 
     if isinstance(url, str):
       if validate.url(url):
@@ -49,8 +90,18 @@ class Document(APObject):
         if isinstance(lnk, Link):
           self.url = url
           break
+        elif isinstance(lnk, str):
+          if validate.url(lnk):
+            self.url = url
+            break
+          else:
+            raise ValueError('Invalid URI link')
+        else:
+          raise TypeConstraintError(nameof(lnk), (Link, str))
     elif isinstance(url, Link):
       self.url = url
+    else:
+      raise TypeConstraintError(nameof(url), (str, list, Link))
 
 class Page(Document):
   def __init__(self, cntxt, identi, name, url):
@@ -67,10 +118,15 @@ class Image(Document):
 class Video(Document):
   def __init__(self, cntxt, identi, name, url, dur):
     super().__init__(cntxt, identi, name, url)
+
     if isinstance(dur, str):
-      xsddurstr = '^P([0-9]+Y)?([0-9]+M)?([0-9]+D)?(T([0-9]+H)?([0-9]+M)?([0-9]+S)?)?$'
+      xsddurstr = '^P([0-9]+Y)?([0-9]{1,2}M)?([0-9]{1,2}D)?(T([0-9]{1,2}H)?([0-9]{1,2}M)?([0-9]{1,2}S)?)?$'
       if match(dur, xsddurstr) is not None:
         self.duration = dur
+      else:
+        raise InvalidXSDError(nameof(dur))
+    else:
+      raise TypeConstraintError(nameof(dur), str)
 
 class Profile(APObject):
   def __init__(self, cntxt, identi, summ, about):
@@ -78,9 +134,15 @@ class Profile(APObject):
 
     if isinstance(summ, str):
       self.summary = summ
+    else:
+      raise TypeConstraintError(nameof(summ), str)
 
-    if isinstance(about, APObject):
+    oftype = (Link, Group, Application, Organization, Person, Service)
+    
+    if isinstance(about, oftype):
       self.describes = about
+    else:
+      raise TypeConstraintError(nameof(about), oftype)
 
     self.possible_attrs += [
       'describes'
@@ -90,14 +152,98 @@ class Relationship(APObject):
   def __init__(self, cntxt, identi, rel, subj, obj):
     super().__init__(cntxt, identi)
 
-    if isinstance(subj, (str, Link, APObject)):
+    oftype = (str, Group, Person, Organization, Application, Service, Link)
+    
+    if isinstance(subj, oftype):
       self.subject = subj
+    else:
+      raise TypeConstraintError(nameof(subj), oftype)
 
-    if isinstance(obj, (str, Link, APObject)):
+    if isinstance(obj, oftype):
       self.object = obj
+    else:
+      raise TypeConstraintError(nameof(obj), oftype)
 
     if isinstance(rel, str):
       self.relationship = rel
+    else:
+      raise TypeConstraintError(nameof(rel), str)
 
-# TODO: Place and Tombstone APObject child classes
-# TODO: Mention Link child class
+class Place(APObject):
+  def __init__(self, cntxt, identi, name, lng, lat, units):
+    super().__init__(cntxt, identi)
+    
+    if isinstance(name, str):
+      self.name = name
+    elif isinstance(name, dict):
+      self.nameMap = {}
+      for k in name:
+        if lang.tag_is_valid(k):
+          self.nameMap[k] = name[k]
+        else:
+          raise ValueError('Invalid language code')
+    else:
+      raise TypeConstraintError(nameof(name), (str, dict))
+
+    if isinstance(lng, float):
+      self.longitude = lng
+    else:
+      raise TypeConstraintError(nameof(lng), float)
+
+    if isinstance(lat, float):
+      self.latitude = lat
+    else:
+      raise TypeConstraintError(nameof(lat), float)
+
+    if isinstance(units, str):
+      self.units = units
+    else:
+      raise TypeConstraintError(nameof(units), str)
+
+    self.possible_attrs += [
+      'longitude', 
+      'latitude',
+      'units',
+      'radius'
+    ]
+
+class Mention(Link):
+  def __init__(self, name, href, rel, w = None, h = None):
+    super().__init__(href, rel, w, h)
+
+    if isinstance(name, str):
+      self.name = name
+    elif isinstance(name, dict):
+      self.nameMap = {}
+      for k in name:
+        if lang.tag_is_valid(k):
+          self.nameMap[k] = name[k]
+        else:
+          raise ValueError('Invalid language code')
+    else:
+      raise TypeConstraintError(nameof(name), (str, dict))
+
+class Tombstone(APObject):
+  def __init__(self, cntxt, identi, former, deltime):
+    super().__init__(cntxt, identi)
+
+    if isinstance(former, APObject):
+      self.formerType = former
+    else:
+      raise TypeConstraintError(nameof(former), APObject)
+
+    if isinstance(deltime, str):
+      xsddtstr = '^(-)?[0-9]+\-[0-9]{1,2}\-[0-9]{1,2}T[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}Z?$'
+      if match(deltime, xsddtstr) is not None:
+        self.deleted = deltime
+      else:
+        raise InvalidXSDError(nameof(deltime))
+    elif isinstance(deltime, dt.datetime):
+      self.deleted = deltime
+    else:
+      raise TypeConstraintError(nameof(deltime), (str, dt.datetime))
+
+    self.possible_attrs += [
+      'formerType',
+      'deleted'
+    ]
